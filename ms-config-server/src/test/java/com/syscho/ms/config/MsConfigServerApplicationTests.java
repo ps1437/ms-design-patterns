43fe253f67e1f8d678ce0036038f3748e91380cd
Praveen Soni <praveen_soni@epam.com>
2025-02-16 17:54:27 +0530

    added Centerlized config server

---
+++ ms-config-server/src/test/java/com/syscho/ms/config/MsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscho.ms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
