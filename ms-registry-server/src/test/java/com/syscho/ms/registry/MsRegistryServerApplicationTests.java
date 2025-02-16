8cf3b348b87d2ee15af4796ab487e344c5cea70f
Praveen Soni <praveen_soni@epam.com>
2025-02-16 17:54:50 +0530

    Added service registry using Eureka

---
+++ ms-registry-server/src/test/java/com/syscho/ms/registry/MsRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscho.ms.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
